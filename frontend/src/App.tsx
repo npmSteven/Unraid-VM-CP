f34f37415b14fd2870b5b3a9673d6ec3f9cd17bd
Steven <28809905+npmSteven@users.noreply.github.com>
2023-02-18 20:54:21 +0700

    Added profile for navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,45 +1,49 @@
 import type { Component } from 'solid-js';
+import { Navbar } from './components/Navbar/Navbar';
 
 import { VMCard } from './components/VMCard/VMCard';
 
 export const App: Component = () => {
   return (
-    <div
-      style={{ display: 'flex' }}
-    >
-      <VMCard
-        status='started'
-        name='Steven-PC'
-        os='Windows 11'
-        storage='1tb'
-        memory='1024M'
-        cpus='6'
-        graphics='RTX3080Ti'
-        ip='192.168.1.150'
-        isAutoStart={false}
-      />
-      <VMCard
-        status='paused'
-        name='Steven-PC'
-        os='Windows 11'
-        storage='1tb'
-        memory='1024M'
-        cpus='6'
-        graphics='RTX3080Ti'
-        ip='192.168.1.150'
-        isAutoStart={false}
-      />
-      <VMCard
-        status='stopped'
-        name='Steven-PC'
-        os='Windows 11'
-        storage='1tb'
-        memory='1024M'
-        cpus='6'
-        graphics='RTX3080Ti'
-        ip='192.168.1.150'
-        isAutoStart={false}
-      />
+    <div>
+      <Navbar />
+      <div
+        style={{ display: 'flex' }}
+      >
+        <VMCard
+          status='started'
+          name='Steven-PC'
+          os='Windows 11'
+          storage='1tb'
+          memory='1024M'
+          cpus='6'
+          graphics='RTX3080Ti'
+          ip='192.168.1.150'
+          isAutoStart={false}
+        />
+        <VMCard
+          status='paused'
+          name='Steven-PC'
+          os='Windows 11'
+          storage='1tb'
+          memory='1024M'
+          cpus='6'
+          graphics='RTX3080Ti'
+          ip='192.168.1.150'
+          isAutoStart={false}
+        />
+        <VMCard
+          status='stopped'
+          name='Steven-PC'
+          os='Windows 11'
+          storage='1tb'
+          memory='1024M'
+          cpus='6'
+          graphics='RTX3080Ti'
+          ip='192.168.1.150'
+          isAutoStart={false}
+        />
+      </div>
     </div>
   );
 };
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,37 @@
 import type { Component } from 'solid-js';
 
+import { FaSolidChevronDown } from 'solid-icons/fa'
+
 import styles from './Navbar.module.css';
 
 export const Navbar: Component = () => {
   return (
-    <div>
-    </div>
+    <nav class={styles.nav}>
+      {/* Left */}
+      <div class={styles.brand}>
+        UNRAID VM CP (Alpha)
+      </div>
+      {/* Right */}
+      <div class={styles.content}>
+        {/* Nav links */}
+        <ul class={styles.links}>
+          <li class={styles.link}>
+            <a href="#">Users</a>
+          </li>
+          <li class={styles.link}>
+            <a href="#">VMs</a>
+          </li>
+        </ul>
+        {/* Profile */}
+        <div class={styles.profile}>
+          {/* Profile Icon */}
+          <img class={styles.profileImage} src="https://ui-avatars.com/api/?name=Steven&size=25" alt="Profile image" />
+          {/* Username */}
+          <span class={styles.username}>Steven</span>
+          {/* Dropdown */}
+          <FaSolidChevronDown size={18} />
+        </div>
+      </div>
+    </nav>
   );
 };
